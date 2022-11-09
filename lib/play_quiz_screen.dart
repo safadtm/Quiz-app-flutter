a3ce1f685770000c5fb4939e24087759e19f233c
safadtm <safadtm2004@gmail.com>
2022-11-09 17:38:44 +0530

    Page view questions created

--- lib/play_quiz_screen.dart
+++ lib/play_quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/const.dart';
 
 class PlayQuizScreen extends StatefulWidget {
   const PlayQuizScreen({super.key});
@@ -10,6 +11,90 @@ class PlayQuizScreen extends StatefulWidget {
 class _PlayQuizScreenState extends State<PlayQuizScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: backgroundColor),
+        backgroundColor: foregroundColor,
+        centerTitle: true,
+        title: const Text(
+          "Play Quiz",
+          style: TextStyle(
+            color: backgroundColor,
+          ),
+        ),
+      ),
+      body: PageView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "This is a Question",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Column(
+                  children: List.generate(
+                    4,
+                    (index) => Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          height: 50,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: foregroundColor,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.all(14),
+                          child: Text(
+                            "Option 1",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: InkWell(
+        onTap: () {},
+        child: Container(
+          height: 70,
+          color: foregroundColor,
+          alignment: Alignment.center,
+          child: const Text(
+            "Next",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+              color: backgroundColor,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
